60d88c145cf0c1089140f3ee11f7feb8184faa76
Rhys Thomas <rhys97@me.com>
2017-11-27 12:07:56 +0000

    Added UML diagrams and minor net updates.

--- net/net.c
+++ net/net.c
@@ -1,28 +1,68 @@
 #include "net.c"
 
-// create NET packet and send to DLL, called by NET?
-error_t net_tx(char* data, char length)
+// add data to the TX buffer
+error_t net_tx(uint8_t* data, char length)
 {
-
+    error_t err;
+    if (!err = buffer_push(net_to_array(*p))) {
+        return ERROR_OK;
+    } else {
+        return err;
+    }
 }
 
-// get NET packet from DLL, called by DLL
-error_t net_rx(char* data, char length)
+// add data to the RX buffer
+error_t net_rx(uint8_t* data, char length)
 {
-    // push data to net_rx_buffer
+
 }
 
 // do stuff with buffers, i.e. if your TX buffer has stuff, pass to DLL.
 void net_tick(void)
 {
-    // check tx buffer
-    if (buffer_size(net_tx_buffer)) {
-        dll_tx(buffer_pop(net_tx_buffer));
+    // state machine?
+    pres_s = next_s;
+    switch (pres_s) {
+        case IDLE:
+            break;
+        case TX:
+            // check tx buffer
+            if (buffer_size(net_tx_buffer)) {
+                dll_tx(buffer_pop(net_tx_buffer));
+            }
+            next_s = IDLE;
+            break;
+        case RX:
+            // check rx buffer
+            if (buffer_size(net_rx_buffer)) {
+                tran_rx(buffer_pop(net_rx_buffer));
+            }
+            next_s = IDLE;
+            break;
+        case OTHER:
+            // other functionality
+            // ...
+            break;
     }
-    // check rx buffer
-    if (buffer_size(net_rx_buffer)) {
-        tran_rx(buffer_pop(net_rx_buffer));
+}
+
+bytestring net_to_array(net_packet_t *p)
+{
+
+}
+
+net_packet_t net_to_struct(uint8_t* data, uint8_t length)
+{
+    net_packet_t packet = {
+        .ack       = (data[0] >> 7) & 0b0001,
+        .type      = (data[0] >> 5) & 0b0011,
+        .hop       = (data[0] >> 2) & 0b0111,
+        .ver       = data[0]        & 0b0011,
+        .src_addr  = data[2],
+        .dest_addr = data[3],
+        .len       = data[4],
+        .tran      = &data[5],
+        .csum      = data[data[4]-2],
     }
-    // other functionality
-    // ...
+    return packet;
 }
--- net/net.h
+++ net/net.h
@@ -27,16 +27,27 @@ typedef struct {
 } net_packet_t;
 
 /* public methods */
-error_t net_tx(char* data, char length);
-error_t net_rx(char* data, char length);
+error_t net_tx(uint8_t* data, uint8_t length);
+error_t net_rx(uint8_t* data, uint8_t length);
 void net_tick();
 
 /* internal functions */
-bytestring net_to_array( net_packet_t p );
-net_packet_t net_to_struct( char* data );
+bytestring net_to_array(net_packet_t *p);
+net_packet_t net_to_struct(uint8_t* data);
 
 /* buffers */
 buffer_t *net_tx_buffer;
 buffer_t *net_rx_buffer;
+
+/* states */
+typedef enum {
+    IDLE,
+    TX,
+    RX,
+    OTHER
+} state_t;
+
+/* present/next states */
+state_t pres_s, next_s;
 
 #endif /* NET_H */
