6fadb6001a379ba6343bcca281efc034ccec6c2f
Rhys Thomas <rhys97@me.com>
2017-12-02 19:23:55 +0000

    Removed version argument, instead use define.

--- net/net-packets.h
+++ net/net-packets.h
@@ -14,48 +14,47 @@
 #include "net.h"
 #include "cksum.h"
 
+#ifndef VERSION
+#define VERSION 0
+#endif // VERSION
+
 /**
     @brief  Create acknoledgement packets.
 
-    @param  v : Version of service.
     @param  src : Address of sender.
     @param  dest : Address of intended receiver.
     @return A NET packet.
 */
-net_packet_t net_ack_packet(uint8_t v, uint8_t src, uint8_t dest);
+net_packet_t net_ack_packet(uint8_t src, uint8_t dest);
 
 /**
     @brief  Create LSA packet for echo message.
 
-    @param  v : Version of service.
     @param  src : Address of sender.
     @return A NET packet.
 */
-net_packet_t net_lsa_packet(uint8_t v, uint8_t src);
+net_packet_t net_lsa_packet(uint8_t src);
 
 /**
     @brief  Create LSP packet.
 
-    @param  v : Version of service.
     @param  src : Address of sender.
     @param  dest : Address of intended receiver.
     @param  table : Link state table of source node.
     @return A NET packet.
     @TODO   Not yet implemented.
 */
-// net_packet_t net_lsp_packet(uint8_t v, uint8_t src, uint8_t dest,
+// net_packet_t net_lsp_packet(uint8_t src, uint8_t dest,
 //     ls_table_t *table);
 
 /**
     @brief  Create broadcast NET packet.
 
-    @param  v : Version of service.
     @param  src : Address of sender.
     @param  data : Byte array of data to send.
     @param  data_len : Length of data array.
     @return A NET packet.
 */
-net_packet_t net_bcast_packet(uint8_t v, uint8_t src, uint8_t *data,
-    uint8_t data_len);
+net_packet_t net_bcast_packet(uint8_t src, uint8_t *data, uint8_t data_len);
 
 #endif // NET_PACKETS_H
